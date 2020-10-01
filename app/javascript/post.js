25cc8dc4d5bad0db8508e933f767fa44a04f475e
katashin <katachanpe0513@icloud.com>
2020-10-01 20:15:11 +0900

    JavaScriptの実装

---
+++ app/javascript/post.js
@@ -0,0 +1,15 @@
+function post() {
+  const itemPrice = document.getElementById("item-price");
+  itemPrice.addEventListener("keyup", () => {
+    const taxPrice = document.getElementById("add-tax-price");
+    const profit = document.getElementById("profit");
+    
+    price = itemPrice.value;
+
+    taxPrice.innerHTML = Math.floor(price * 0.1);
+    profit.innerHTML = Math.floor(price - (price * 0.1));
+    
+  });
+}
+
+setInterval(post,1000);

a54f25e6e945f20e4994fd2dccab34e80cd4ee01
katashin <katachanpe0513@icloud.com>
2020-10-03 07:39:09 +0900

    validatesの再修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -9,19 +9,13 @@ class Item < ApplicationRecord
     validates :image
     validates :name
     validates :info
-    validates :price
+    validates :category_id, numericality: { other_than: 1 }
+    validates :sales_status_id, numericality: { other_than: 1 }
+    validates :shapping_fee_status_id, numericality: { other_than: 1 }
+    validates :prefecture_id, numericality: { other_than: 1 }
+    validates :scheduled_delivery_id, numericality: { other_than: 1 }
+    validates :price, format: { with: /\A[0-9]+\z/ }, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
   end
-
-  with_options numericality: { other_than: 1 } do
-    validates :category_id
-    validates :sales_status_id
-    validates :shapping_fee_status_id
-    validates :prefecture_id
-    validates :scheduled_delivery_id
-  end
-
-  validates :price, format: { with: /\A[0-9]+\z/ }
-  validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
 
   belongs_to :user
   has_one_attached :image

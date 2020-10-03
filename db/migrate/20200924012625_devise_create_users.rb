bd5d3bfdf88a3d138a7aaf6c23ee22f7156958fa
katashin <katachanpe0513@icloud.com>
2020-10-03 16:26:48 +0900

    Update 20200924012625_devise_create_users.rb

--- db/migrate/20200924012625_devise_create_users.rb
+++ db/migrate/20200924012625_devise_create_users.rb
@@ -11,11 +11,11 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
       t.string  :last_name_kana,    null: false
       t.date    :birth_date,        null: false
 
-      t.string :email,              null: false, default: ""
-      t.string :encrypted_password, null: false, default: ""
+      t.string :email,              null: false, default: "", limit: 191
+      t.string :encrypted_password, null: false, default: "", limit: 191
 
       ## Recoverable
-      t.string   :reset_password_token
+      t.string   :reset_password_token, limit: 191
       t.datetime :reset_password_sent_at
 
       ## Rememberable

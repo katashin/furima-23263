658a6bd82b028c54ca98617b11c0c32e981126b9
katashin <katachanpe0513@icloud.com>
2020-10-10 18:47:11 +0900

    商品投稿一覧表示

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,10 @@
 class ItemsController < ApplicationController
   def index
+<<<<<<< Updated upstream
     @items = Item.order('created_at DESC')
+=======
+    @items = Item.order("created_at DESC")
+>>>>>>> Stashed changes
   end
 
   def new

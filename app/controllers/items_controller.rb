178bfd5f1a57982acf753cf0a8780ccd99110f4d
katashin <katachanpe0513@icloud.com>
2020-10-11 17:35:34 +0900

    修正①

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -17,7 +17,6 @@ class ItemsController < ApplicationController
   end
 
   def show
-    # binding.pry
     @item = Item.find(params[:id])
     @user = User.find(@item.user_id)
     @category = Category.find(@item.category_id)

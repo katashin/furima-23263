c8b8b0c7620740b8473cd95e752f0dda13d977b7
katashin <katachanpe0513@icloud.com>
2020-10-11 18:18:44 +0900

    修正②

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -18,12 +18,6 @@ class ItemsController < ApplicationController
 
   def show
     @item = Item.find(params[:id])
-    @user = User.find(@item.user_id)
-    @category = Category.find(@item.category_id)
-    @sales_status = SalesStatus.find(@item.sales_status_id)
-    @prefecture = Prefecture.find(@item.prefecture_id)
-    @shapping_fee_status = ShappingFeeStatus.find(@item.shapping_fee_status_id)
-    @scheduled_delivery = ScheduledDelivery.find(@item.scheduled_delivery_id)
   end
 
   private

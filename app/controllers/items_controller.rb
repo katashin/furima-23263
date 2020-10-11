af8b9e41ca2ca80b58d65aedca8b857b1c8cf753
katashin <katachanpe0513@icloud.com>
2020-10-11 16:25:17 +0900

    全実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -16,6 +16,17 @@ class ItemsController < ApplicationController
     end
   end
 
+  def show
+    # binding.pry
+    @item = Item.find(params[:id])
+    @user = User.find(@item.user_id)
+    @category = Category.find(@item.category_id)
+    @sales_status = SalesStatus.find(@item.sales_status_id)
+    @prefecture = Prefecture.find(@item.prefecture_id)
+    @shapping_fee_status = ShappingFeeStatus.find(@item.shapping_fee_status_id)
+    @scheduled_delivery = ScheduledDelivery.find(@item.scheduled_delivery_id)
+  end
+
   private
 
   def item_params
